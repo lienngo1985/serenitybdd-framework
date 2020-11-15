1174b8257f86190ce57158fece4163326478298c
lienngo1985 <ana.ngo.1985@gmail.com>
2020-11-15 22:56:09 +0700

    Update serenity.conf to use default url

--- src/test/java/net/serenitybdd/screenplay/amazon/pages/HomePage.java
+++ src/test/java/net/serenitybdd/screenplay/amazon/pages/HomePage.java
@@ -1,9 +1,8 @@
 package net.serenitybdd.screenplay.amazon.pages;
 
 import net.serenitybdd.core.pages.PageObject;
-import net.thucydides.core.annotations.DefaultUrl;
 
-@DefaultUrl("https://www.amazon.com")
+//@DefaultUrl("https://www.amazon.com")
 public class HomePage extends PageObject {
 
 }
